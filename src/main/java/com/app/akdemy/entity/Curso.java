06e399b71378467ad23e3f8220625c9e775d4ab5
Julian Manosalva <jmanosalva@unal.edu.co>
2022-04-13 17:14:06 -0500

    Tabla Curso_Estudiante agregada

--- src/main/java/com/app/akdemy/entity/Curso.java
+++ src/main/java/com/app/akdemy/entity/Curso.java
@@ -1,6 +1,7 @@
 package com.app.akdemy.entity;
 
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -19,6 +22,12 @@ public class Curso {
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
 
+    @Column
+    private String Nombre_Curso;
+
+    @Column
+    private Integer Anio_Curso;
+
     @Column 
 	private String descripcion;
 
@@ -26,8 +35,10 @@ public class Curso {
     public Curso() {
     }
 
-    public Curso(long id, String descripcion) {
+    public Curso(long id, String Nombre_Curso, Integer Anio_Curso, String descripcion) {
         this.id = id;
+        this.Anio_Curso = Anio_Curso;
+        this.Nombre_Curso = Nombre_Curso;
         this.descripcion = descripcion;
     }
 
@@ -37,12 +48,36 @@ public class Curso {
     @JoinColumn(name = "Director_Curso", updatable = false, nullable = false)
     private Profesor profesor;
 
+    @ManyToMany
+    @JoinTable(name = "Curso_Estudiante"
+            , joinColumns = @JoinColumn(name = "Id_Estudiante")
+            , inverseJoinColumns = @JoinColumn(name = "Id_Curso"))
+    private Set<Curso> curso;
+
+    //Getter y Setter
+
     public long getId() {
         return this.id;
     }
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getNombre_Curso() {
+        return this.Nombre_Curso;
+    }
+
+    public void setNombre_Curso(String Nombre_Curso) {
+        this.Nombre_Curso = Nombre_Curso;
+    }
+
+    public Integer getAnio_Curso() {
+        return this.Anio_Curso;
+    }
+
+    public void setAnio_Curso(Integer Anio_Curso) {
+        this.Anio_Curso= Anio_Curso;
     }
 
     public String getDescripcion() {
@@ -52,6 +87,7 @@ public class Curso {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+    
 
 
     @Override
@@ -62,18 +98,19 @@ public class Curso {
             return false;
         }
         Curso curso = (Curso) o;
-        return id == curso.id && Objects.equals(descripcion, curso.descripcion);
+        return id == curso.id && Objects.equals(descripcion, curso.descripcion) && Objects.equals(Anio_Curso, curso.Anio_Curso) && Objects.equals(Nombre_Curso, curso.Nombre_Curso);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, descripcion);
+        return Objects.hash(id, Nombre_Curso, Anio_Curso, descripcion);
     }
 
     @Override
     public String toString() {
         return "{" +
             " id='" + getId() + "'" +
+            ", nombre='" + getDescripcion() + "'" +
             ", descripcion='" + getDescripcion() + "'" +
             "}";
     }
