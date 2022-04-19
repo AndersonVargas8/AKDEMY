9a78d63666b9136404fc881b9089ff75f3596de8
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-04-18 19:13:16 -0500

    Frontend Profesores v2

--- src/main/resources/static/js/profesores.js
+++ src/main/resources/static/js/profesores.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-    $('#example').DataTable(
+    $('#teachers').DataTable(
         {
             language: {
                 "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
 
+            },
+            "columnDefs": [ {
+                "targets": -1,
+                "data": null,
+                "defaultContent": "<button type='button' class='btn btn-danger btn-sm'><span class='material-icons align-middle'>delete</span><span class='align-middle'>Eliminar</span></button>"
+            },
+            {
+                "targets": -2,
+                "data": null,
+                "defaultContent": "<button type='button' class='btn btn-info btn-sm'><span class='material-icons align-middle'>edit</span><span class='align-middle'>Editar</span></button>"
             }
+        ]
         }
     );
     
