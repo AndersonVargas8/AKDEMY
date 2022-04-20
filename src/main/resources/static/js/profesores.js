514e50538ed301c44c4746040810f0282ea01138
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-04-19 21:23:47 -0500

    Correción por nuevo modelo BD

--- src/main/resources/static/js/profesores.js
+++ src/main/resources/static/js/profesores.js
@@ -3,19 +3,7 @@ $(document).ready(function() {
         {
             language: {
                 "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-
             },
-            "columnDefs": [ {
-                "targets": -1,
-                "data": null,
-                "defaultContent": "<button type='button' class='btn btn-danger btn-sm'><span class='material-icons align-middle'>delete</span><span class='align-middle'>Eliminar</span></button>"
-            },
-            {
-                "targets": -2,
-                "data": null,
-                "defaultContent": "<button type='button' class='btn btn-info btn-sm'><span class='material-icons align-middle'>edit</span><span class='align-middle'>Editar</span></button>"
-            }
-        ]
         }
     );
     
