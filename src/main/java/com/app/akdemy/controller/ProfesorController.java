284b9185cbb8c7663361a2fb19259fbb097e5d12
AndersonVargas8 <anvargasa@unal.edu.co>
2022-04-20 21:16:49 -0500

    link profesores en el navbar coordinador

--- src/main/java/com/app/akdemy/controller/ProfesorController.java
+++ src/main/java/com/app/akdemy/controller/ProfesorController.java
@@ -1,7 +1,5 @@
 package com.app.akdemy.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.validation.Valid;
 
 import com.app.akdemy.entity.Profesor;
@@ -9,11 +7,10 @@ import com.app.akdemy.interfacesServices.IProfesorService;
 import com.app.akdemy.repository.ProfesorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -24,8 +21,9 @@ public class ProfesorController {
 
     @Autowired
     private ProfesorRepository repProfesor;
-
-    @GetMapping("/profesores")
+    
+    @GetMapping("/coordinador/profesores")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_COORDINADOR')")
     public String index(Model model) {
 
         model.addAttribute("nuevoProfesor", new Profesor());
