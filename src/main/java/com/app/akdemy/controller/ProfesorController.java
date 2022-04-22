f1dc05c4dd565cf6adafd8670ae8f371d509fea0
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-04-21 21:28:36 -0500

    Create Profesor

--- src/main/java/com/app/akdemy/controller/ProfesorController.java
+++ src/main/java/com/app/akdemy/controller/ProfesorController.java
@@ -2,15 +2,19 @@ package com.app.akdemy.controller;
 
 import javax.validation.Valid;
 
+import com.app.akdemy.Exception.UsernameOrIdNotFound;
 import com.app.akdemy.entity.Profesor;
+import com.app.akdemy.entity.User;
 import com.app.akdemy.interfacesServices.IProfesorService;
 import com.app.akdemy.repository.ProfesorRepository;
+import com.app.akdemy.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -21,20 +25,27 @@ public class ProfesorController {
 
     @Autowired
     private ProfesorRepository repProfesor;
+
+    @Autowired
+    private UserService serUser;
+
     
     @GetMapping("/coordinador/profesores")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_COORDINADOR')")
     public String index(Model model) {
 
-        model.addAttribute("nuevoProfesor", new Profesor());
+        model.addAttribute("profesor", new Profesor());
         model.addAttribute("profesores", repProfesor.findAll());
+        model.addAttribute("users", serUser.getAllUsers());
         model.addAttribute("itemNavbar","profesores");
         return "coordinador/profesores/index";
     }
 
     @PostMapping("/saveprofesor")
-    public String createProfesor(@Valid Profesor profesor, Model model) {
+    public String createProfesor(@Valid @ModelAttribute("profesor")Profesor profesor, Model model) throws UsernameOrIdNotFound {
+
+        profesor.setUsuario(serUser.getUserById(profesor.getUsuario().getId()));
         service.saveProfesor(profesor);
-        return "redirect:/profesores";
+        return "redirect:/coordinador/profesores";
     }
 }
