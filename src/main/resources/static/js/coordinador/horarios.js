722155b14be2fdde95244df15bbd4c2111df1544
AndersonVargas8 <anvargasa@unal.edu.co>
2022-05-04 00:15:18 -0500

    Se crea la funcionalidad de agregar materias al horario

--- src/main/resources/static/js/coordinador/horarios.js
+++ src/main/resources/static/js/coordinador/horarios.js
@@ -4,13 +4,33 @@ $(document).ready(function () {
         let url = "/coordinador/consultaHorario/" + idCurso;
         $("#seccionHorario").load(url);
     });
+    $("#agregarMatHoras").change(function () {
+        let horas = $("#agregarMatHoras").val();
+        let horaInicio = $("#agregarMatHorIni").val().substring(0,2);
+        let horaFin = 0 + parseInt(horaInicio) + parseInt(horas);
+        if(horaFin<10){
+            horaFin = "0" + horaFin;
+        }
+        $("#agregarMatHorFin").val(horaFin + ":00:00");
+    });
 });
 
-function nuevoHorario(diaHora) {
+function nuevoHorario(curso,dia,hora) {
     try {
-        let id = document.getElementById(diaHora).value;
+        let id = document.getElementById(dia+hora).value;
     } catch {
-        alert("nuevo horario " + diaHora);
+        $("#agregarMatCurso").val(curso);
+        $("#agregarMatDia").val(dia);
+        $("#agregarMatHorIni").val(hora+":00:00");
+        let horaFin = 0 + parseInt(hora) + parseInt(1);
+        if(horaFin<10){
+            horaFin = "0" + horaFin;
+        }
+        $("#agregarMatHorFin").val(horaFin + ":00:00");
+        $("#agregarMatMateria").get(0).selectedIndex = 0;
+        $("#agregarMatProfesor").get(0).selectedIndex = 0;
+        $("#agregarMatHoras").val(1);
+        $('#modalAgregarMateria').modal();
     }
 }
 
@@ -39,4 +59,29 @@ function confirmarEliminarHorario() {
 function abrirInfoMateria() {
     $('#deleteModalHorario').modal('hide');
     $('#modalInfoMaterias').modal();
+}
+
+function guardarHorario(){
+    let idCurso = $("#selCurso").val();
+    let dia = $("#agregarMatDia").val();
+    let horaInicio = $("#agregarMatHorIni").val();
+    let horaFin = $("#agregarMatHorFin").val();
+    let idMateria = $("#agregarMatMateria").val();
+    let idProfesor = $("#agregarMatProfesor").val();
+
+    
+    if(idProfesor == ""){    
+        $("#agregarMatProfesor").focus();
+    }
+    
+    if(idMateria == ""){    
+        $("#agregarMatMateria").focus();
+    }
+    if(idMateria != "" && idProfesor != ""){
+        let url = "/coordinador/agregarHorario/" + idCurso + '/' + dia + '/' + horaInicio + '/' + horaFin + '/' + idMateria + '/' + idProfesor;
+        $("#seccionHorario").load(url);
+        $('#modalAgregarMateria').modal('hide');
+    }
+
+
 }
