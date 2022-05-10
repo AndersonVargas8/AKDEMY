8a76d29b55421a0ce715be3cf158328beb757c36
AndersonVargas8 <anvargasa@unal.edu.co>
2022-05-10 17:20:57 -0500

    Correciones de algunos problemas

--- src/main/resources/static/js/coordinador/cursos.js
+++ src/main/resources/static/js/coordinador/cursos.js
@@ -15,11 +15,11 @@ function verEstudiantes(id){
 
 function editarCurso(id){
     // let mes = document.getElementById("selMesCalendar").value;
-    $('#modalLoading').modal({
-        backdrop: "static", //remove ability to close modal with click
-        keyboard: false, //remove option to close with keyboard
-        show: true //Display loader!
-      });
+    // $('#modalLoading').modal({
+    //     backdrop: "static", //remove ability to close modal with click
+    //     keyboard: false, //remove option to close with keyboard
+    //     show: true //Display loader!
+    //   });
     var url = "/coordinador/cursos/" + id;
     $("#formEditarCursos").load(url, function(){
         $('#modalLoading').modal('hide');
