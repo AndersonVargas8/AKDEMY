40e442bc85ac3bb99029e880b8158283629f9b73
AndersonVargas8 <anvargasa@unal.edu.co>
2022-05-21 22:43:16 -0500

    Intento #3 despliegue postgres -solucion 2 error user

--- src/main/java/com/app/akdemy/entity/User.java
+++ src/main/java/com/app/akdemy/entity/User.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 
